40ea13f75f9187f74acc6984c31a300cbf18924a
IrinaMa72 <morevaia72@gmail.com>
2023-06-13 23:26:22 +0500

    первый коммит

---
+++ src/test/java/com/example/hwcalculator/HwCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
